320eff65018c5746ebc68cbbeda6ad46473bad1f
Vanessa <vkwacko007@gmail.com>
2022-03-28 19:42:15 +0100

    separate functions into separate script

--- quality_standard.R
+++ quality_standard.R
@@ -2,10 +2,15 @@
 library(dplyr)
 library(stringr)
 library(tidyr)
+library(readr)
+
 library(rvest)
 library(rlang)
 
+source('./src/qs_functions.R')
+
 qs_number <- 205
+qs_id <- paste0("QS", qs_number)
 qs_url <- paste0("https://www.nice.org.uk/guidance/qs", qs_number)
 
 # Returns xml_document object with webpage html
@@ -16,6 +21,25 @@ qs_name <- qs_html %>%
     html_elements("#content-start") %>% 
     html_text2()
 
+qs_directory <- tibble(qs_id = qs_id, qs_name = qs_name)
+
+# Create output folder if it doesn't already exist
+if(!dir.exists("output")) {
+  dir.create("output")
+}
+
+# Create qs_directory or append new QS, if it doesn't already exist in the table
+if(!file.exists("./output/qs_directory.csv")) {
+  # Create empty and write empty table
+  write_csv(qs_directory, "./output/qs_directory.csv")
+} else {
+  qs_dir_import <- read_csv("./output/qs_directory.csv")
+  
+  if (!(qs_id %in% qs_dir_import$qs_id)) {
+    write_csv(qs_directory, "./output/qs_directory.csv", append = T)
+  }
+}
+
 # Extract links to quality statements within quality standard
 qs_links <- qs_html %>% 
     html_elements("#Guidance-Menu a") %>% 
@@ -25,153 +49,25 @@ qs_links <- qs_html %>%
 qs_links <- qs_links[str_detect(qs_links, "Quality-statement-\\d+")]
 
 # Read quality statement
-extract_statement <- function(qs_links, n, qs_number) {
-  qs <- read_html(qs_links[n])
-  
-  # assumes only one paragraph as using html_element() not elements
-  title <- qs %>% 
-      html_element(".h2") %>% 
-      html_text2()
-  
-  statement_number <- str_extract(title, "(?<=Quality statement )\\d+(?=:)")
-  
-  statement <- qs %>% 
-      html_element('div[title="Quality statement"] p') %>% 
-      html_text2() %>% 
-      # Remove bracketed info on year
-      str_remove("\\[.*\\]") %>% 
-      str_trim()
-  
-  # Extract all p elements inside a div element with title attribute "Structure" which is itself inside a div element with title attribute "Quality measures"
-  qm_structure <- qs %>% 
-      html_elements('div[title="Quality measures"] div[title="Structure"] p') %>% 
-      html_text2() %>% 
-      # Remove carriage return control character
-      str_replace_all("\r", "") %>% 
-      # trim white space
-      str_trim() 
-  
-  qm_process <- qs %>% 
-      html_elements('div[title="Quality measures"] div[title="Process"] p') %>% 
-      html_text2() %>% 
-      str_replace_all("\r", "") %>% 
-      str_trim()
-  
-  qm_outcome <- qs %>% 
-      html_elements('div[title="Quality measures"] div[title="Outcome"] p') %>% 
-      html_text2() %>% 
-      str_replace_all("\r", "") %>% 
-      str_trim()
-  
-  # Separate out points for structure, process and outcome
-  section_table <- function(section) {
-    
-    if (!is_empty(section))  {
-        # Generate empty table
-        table <- data.frame(point = rep(NA, length(section)), text = section)
-        
-        # If text in text column starts with a lower case character followed by a bracket, extract that letter into point column (e.g. "a)")
-        table <- table %>% 
-            mutate(point = if_else(
-                str_detect(text, "^[:lower:]{1}\\) "),
-                str_extract(text, "^[:lower:]{1}(?=\\) )"),
-                NULL))
-        
-        # If for the first row, there is no a), b) etc, assign "a" in point column
-        if (is.na(table$point[1])) {
-            table$point[1] <- "a"
-        }
-        
-        # If a row has no value in the point column, assign it the point letter for the row above
-        for (i in seq_along(table$point)) {
-            if (is.na(table$point[i])) {
-                table$point[i] <- table$point[i-1]
-            }
-        }
-        
-        # E
-        table <- table %>% 
-            group_by(point) %>% 
-            # Assign index to rows within each point
-            mutate(point_index = 1:n()) %>% 
-            ungroup() %>% 
-            # Create label column and assign
-            mutate(label = case_when(
-                str_detect(text, "^[:lower:]{1}\\) ") ~ "statement",
-                str_detect(text, "^Data source: ") ~ "data source",
-                str_detect(text, "^Numerator \u2013") ~ "numerator",
-                str_detect(text, "^Denominator \u2013") ~ "denominator")) %>% 
-            # If no label and first statement in point group, assign "statement" label
-            mutate(label = if_else(
-                is.na(label) & point_index == 1,
-                "statement",
-                label)) %>% 
-            # Drop index column
-            select(-point_index)
-        
-        return(table)
-    }
-    
-    else {
-      table <- data.frame(point = character(), text = character(), label = character())
-      
-      return(table)
-    }
-  }
-  
-  qm_structure_table <- section_table(qm_structure) %>% 
-      mutate(qs_number = paste0("QS", qs_number),
-             statement_number = statement_number,
-             measure = "structure",
-             .before = 1
-      )
-  
-  
-  qm_process_table <- section_table(qm_process)  %>% 
-      mutate(qs_number = paste0("QS", qs_number),
-             statement_number = statement_number,
-             measure = "process",
-             .before = 1
-      )
-  
-  
-  qm_outcome_table <- section_table(qm_outcome)  %>% 
-      mutate(qs_number = paste0("QS", qs_number),
-             statement_number = statement_number,
-             measure = "outcome",
-             .before = 1
-      )
-  
-  # Combine measures
-  measures <- rbind(qm_structure_table, qm_process_table, qm_outcome_table) %>% 
-      filter(label != "data source") %>% 
-      mutate(text = str_remove(text, "^.*(:|\\)|\u2013)") %>% 
-                 str_to_sentence() %>%
-                 str_trim()) %>% 
-      relocate(text, .after = last_col()) %>% 
-      mutate(measure_id = paste(statement_number, measure, point, sep = "-")) %>% 
-      pivot_wider(names_from = label,
-                  values_from = text)
-  
-  if(!("numerator" %in% colnames(measures))) {
-    measures$numerator <- NA
-    measures$denominator <- NA
-  }
-  
-  return(measures)
-}
 
-all_statements <- data.frame(qs_number = character(), 
+statement_text <- data.frame(qs_id = character(),
                              statement_number = numeric(),
-                             measure = character(),
+                             statement = character())
+
+all_statements <- data.frame(statement_number = numeric(),
+                             measure_type = character(),
                              point = character(),
                              measure_id = character(),
-                             statement = character(),
+                             measure = character(),
                              numerator = character(),
                              denominator = character())
 
 for (i in seq_along(qs_links)) {   
-    st_table  <- extract_statement(qs_links, i, qs_number)
+    st_table  <- extract_statement(qs_links, i, qs_id)
     
-    all_statements <- rbind(all_statements, st_table)
+    statement_text <- rbind(statement_text, st_table$statement_row)
+    all_statements <- rbind(all_statements, st_table$measures)
 }
+
+write_csv(statement_text, sprintf("./output/%s_statements.csv", qs_id))
+write_csv(all_statements, sprintf("./output/%s_measures.csv", qs_id))
---
+++ src/qs_functions.R
@@ -0,0 +1,139 @@
+extract_measure <- function(qs_html, measure_type) {
+    
+    css <- str_c('div[title="Quality measures"] div[title="', measure_type, '"] p')
+    
+    quality_measure <- qs_html %>% 
+        html_elements(css) %>% 
+        html_text2() %>% 
+        # Remove carriage return control character
+        str_replace_all("\r", "") %>% 
+        # trim white space
+        str_trim() 
+    
+    return(quality_measure)
+}
+
+section_table <- function(section) {
+    
+    if (!is_empty(section))  {
+        # Generate empty table
+        table <- data.frame(point = rep(NA, length(section)), measure = section)
+        
+        # If measure in measure column starts with a lower case character followed by a bracket, extract that letter into point column (e.g. "a)")
+        table <- table %>% 
+            mutate(point = if_else(
+                str_detect(measure, "^[:lower:]{1}\\) "),
+                str_extract(measure, "^[:lower:]{1}(?=\\) )"),
+                NULL))
+        
+        # If for the first row, there is no a), b) etc, assign "a" in point column
+        if (is.na(table$point[1])) {
+            table$point[1] <- "a"
+        }
+        
+        # If a row has no value in the point column, assign it the point letter for the row above
+        for (i in seq_along(table$point)) {
+            if (is.na(table$point[i])) {
+                table$point[i] <- table$point[i-1]
+            }
+        }
+        
+        # E
+        table <- table %>% 
+            group_by(point) %>% 
+            # Assign index to rows within each point
+            mutate(point_index = 1:n()) %>% 
+            ungroup() %>% 
+            # Create label column and assign
+            mutate(label = case_when(
+                str_detect(measure, "^[:lower:]{1}\\) ") ~ "statement",
+                str_detect(measure, "^Data source: ") ~ "data source",
+                str_detect(measure, "^Numerator \u2013") ~ "numerator",
+                str_detect(measure, "^Denominator \u2013") ~ "denominator")) %>% 
+            # If no label and first statement in point group, assign "statement" label
+            mutate(label = if_else(
+                is.na(label) & point_index == 1,
+                "statement",
+                label)) %>% 
+            # Drop index column
+            select(-point_index)
+        
+        return(table)
+    }
+    
+    else {
+        table <- data.frame(point = character(), measure = character(), label = character())
+        
+        return(table)
+    }
+}
+
+extract_statement <- function(qs_links, n, qs_id) {
+    qs_html <- read_html(qs_links[n])
+    
+    # assumes only one paragraph as using html_element() not elements
+    title <- qs_html %>% 
+        html_element(".h2") %>% 
+        html_text2()
+    
+    statement_number <- str_extract(title, "(?<=Quality statement )\\d+(?=:)")
+    
+    statement <- qs_html %>% 
+        html_element('div[title="Quality statement"] p') %>% 
+        html_text2() %>% 
+        # Remove bracketed info on year
+        str_remove("\\[.*\\]") %>% 
+        str_trim()
+    
+    statement_row <- tibble(qs_id = qs_id, statement_number = statement_number, statement = statement)
+    
+    # Extract all p elements inside a div element with title attribute "Structure" which is itself inside a div element with title attribute "Quality measures"
+    qm_structure <- extract_measure(qs_html, measure = "Structure")
+    qm_process <- extract_measure(qs_html, measure = "Process")
+    qm_outcome <- extract_measure(qs_html, measure = "Outcome")
+    
+    # Separate out points for structure, process and outcome
+    qm_structure_table <- section_table(qm_structure) %>% 
+        mutate(qs_id = qs_id,
+               statement_number = statement_number,
+               measure_type = "structure",
+               .before = 1
+        )
+    
+    
+    qm_process_table <- section_table(qm_process)  %>% 
+        mutate(qs_id = qs_id,
+               statement_number = statement_number,
+               measure_type = "process",
+               .before = 1
+        )
+    
+    
+    qm_outcome_table <- section_table(qm_outcome)  %>% 
+        mutate(qs_id = qs_id,
+               statement_number = statement_number,
+               measure_type = "outcome",
+               .before = 1
+        )
+    
+    # Combine measures
+    measures <- rbind(qm_structure_table, qm_process_table, qm_outcome_table) %>% 
+        filter(label != "data source") %>% 
+        mutate(measure = str_remove(measure, "^.*(:|\\)|\u2013)") %>% 
+                   str_to_sentence() %>%
+                   str_trim()) %>% 
+        relocate(measure, .after = last_col()) %>% 
+        mutate(measure_id = paste(qs_id, statement_number, measure_type, point, sep = "-")) %>% 
+        pivot_wider(names_from = label,
+                    values_from = measure) %>% 
+        rename(measure = statement) %>% 
+        select(-qs_id)
+    
+    if(!("numerator" %in% colnames(measures))) {
+        measures$numerator <- NA
+        measures$denominator <- NA
+    }
+    
+    return(list("statement_row" = statement_row,
+                "measures" = measures))
+}
